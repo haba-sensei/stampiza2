3e17172236bab0daa63627609638e028ce3945c6
Jorge Acosta <haba.dev.oficial@gmail.com>
2024-01-30 03:24:53 -0500

    cambios para despliege

--- src/config/mongoConfig.ts
+++ src/config/mongoConfig.ts
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose';
 
-// export const mongoURI = 'mongodb://localhost:27017/stampiza2_tshirt';
-export const mongoURI = 'mongodb+srv://haba:%402582Jack@cluster0.e8hmy59.mongodb.net';
-
 export const connectToMongoDB = async () => {
     try {
+        const mongoURI = process.env.MONGOURI ? process.env.MONGOURI : 'mongodb://localhost:27017/stampiza2_tshirt';
         await mongoose.connect(mongoURI);
-        console.log("Conexión exitosa a MongoDB");
+        console.log("Conexión exitosa a MongoDB:::", mongoURI);
         return true;
     } catch (error) {
         console.error('Error al conectar a MongoDB:', error);
