520609d2818c44f0b3c1586c3148f83b69071370
Jorge Acosta <haba.dev.oficial@gmail.com>
2024-01-30 05:37:31 -0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import dotenv from 'dotenv';
 import express, { Express } from 'express';
+import 'dotenv/config';
 import 'module-alias/register';
 import routes from "./routes";
 import cors from 'cors';
 import { errorMiddleware } from './middlewares/errorMiddleware';
 import { connectToMongoDB } from './config/mongoConfig';
 
-dotenv.config();
+
 const app: Express = express();
 app.use(express.json());
 app.use(routes);
